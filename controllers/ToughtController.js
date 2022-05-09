e6018ef58eba61836e4a33ed988c0235ed0f3afc
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-05-09 13:00:44 -0300

    criado metodo para busca, ordenação e finalização do css

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,17 +1,42 @@
 const Tought = require('../models/Tought')
 const User = require('../models/User')
+const { Op } = require('sequelize')
 
 module.exports = class ToughtController {
 
     static async showToughts(req, res) {
 
+        let search = ''
+
+        if(req.query.search){
+            search = req.query.search
+        }
+
+        let order = 'DESC'
+
+        if(req.query.order === 'old'){
+            order = 'ASC'
+        } else {
+            order = 'DESC'
+        }
+
         const toughtsData = await Tought.findAll({
             include:User,
+            where: {
+                title: {[Op.like]: `%%${search}`},
+            },
+            order: [['createdAt', order]],
         })
         
         const toughts = toughtsData.map((result) => result.get({ plain:true }))
 
-        res.render('toughts/home', { toughts })
+        let toughtsQty = toughts.length
+
+        if(toughtsQty === 0){
+            toughtsQty = false
+        }
+
+        res.render('toughts/home', { toughts, search, toughtsQty })
     }
     
     static async dashboard(req, res){
